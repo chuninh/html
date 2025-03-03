94f1cfe2597d2730c3e5c97d98bc872b8c2b40cd
chuninh <chuninh2006@gmail.com>
2025-03-03 15:42:09 +0700

    Add files via upload

---
+++ btss19/bt7.js
@@ -0,0 +1,5 @@
+let firstnumber = Number(prompt("Hãy nhập số thứ nhất: "));
+let secondnumber = Number(prompt("Hãy nhập số thứ hai: "));
+let thirdnumber = Number(prompt("Hãy nhập số thứ ba: "));
+let highestnumber = Math.max(firstnumber,secondnumber,thirdnumber);
+alert("so lon nhat la"+highestnumber);
