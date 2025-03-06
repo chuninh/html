43ae7534908076121a5851982bc023936862fa0f
chuninh <chuninh2006@gmail.com>
2025-03-06 17:29:34 +0700

    Add files via upload

---
+++ btss22/bt4.js
@@ -0,0 +1,15 @@
+let numbers = prompt("moi ban nhap day so bat ky");
+arr = numbers.split("");
+let max = arr[0];
+console.log(arr);  
+if (!isNaN(numbers)) {
+    for (i = 0; i < numbers.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    console.log(max + "la so lon nhat trong day");
+}else{
+    console.log("ko hop le");
+}
+
